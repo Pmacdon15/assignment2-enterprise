9fb4e4059de46fd922951e9481c2e03e993f371e
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-24 13:39:46 -0600

    register - updated code to work with new schema

--- register.php
+++ register.php
@@ -3,12 +3,18 @@ include "config.php";
 
 $data = json_decode(file_get_contents("php://input"));
 
-if(!empty($data->username) && !empty($data->password)){
+
+
+if(!empty($data->email) && !empty($data->password) && !empty($data->role_id)){
     $hashedPassword = password_hash($data->password, PASSWORD_BCRYPT);
-    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (:username, :password)");
+    $stmt = $conn->prepare("INSERT INTO users (email, password, role_id) VALUES (:email, :password, :role_id)");
 
     try {
-        $stmt->execute(["username" => $data->username, "password" => $hashedPassword]);
+        $stmt->execute([
+            "email" => $data->email, 
+            "password" => $hashedPassword, 
+            "role_id" => $data->role_id
+        ]);
         echo json_encode(["message" => "User registered successfully"]);
     } 
     catch (PDOException $e) {
@@ -16,7 +22,7 @@ if(!empty($data->username) && !empty($data->password)){
     }
 
 }else{
-    echo json_encode(["error" => "User and password are required."]);
+    echo json_encode(["error" => "email ,password and role id are required."]);
 }
 
 ?>
