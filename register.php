eebf7eccd32835e202e00533ae7c586da6def5b5
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-24 18:13:09 -0600

    logging - cleaned up error log message

--- register.php
+++ register.php
@@ -3,23 +3,22 @@ include "config.php";
 
 $data = json_decode(file_get_contents("php://input"));
 
-if(!empty($data->email) && !empty($data->password) && !empty($data->role_id)){
+if (!empty($data->email) && !empty($data->password) && !empty($data->role_id)) {
     $hashedPassword = password_hash($data->password, PASSWORD_BCRYPT);
     $stmt = $conn->prepare("INSERT INTO users (email, password, role_id) VALUES (:email, :password, :role_id)");
 
     try {
         $stmt->execute([
-            "email" => $data->email, 
-            "password" => $hashedPassword, 
+            "email" => $data->email,
+            "password" => $hashedPassword,
             "role_id" => $data->role_id
         ]);
         echo json_encode(["message" => "User registered successfully"]);
-    } 
-    catch (PDOException $e) {
-        echo json_encode(["error" => "User already exists"]);
+    } catch (PDOException $e) {
+        echo json_encode(["error" => "User not added " . $e ->getMessage()]);
     }
 
-}else{
+} else {
     echo json_encode(["error" => "email ,password and role id are required."]);
 }
 
