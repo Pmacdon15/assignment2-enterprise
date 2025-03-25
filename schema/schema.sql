74976df07f435101db8181aa4ff6ae0864c5ebae
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-24 18:12:17 -0600

    schema - schema now restricts fk values that are null

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,7 +1,12 @@
+DROP TABLE IF EXISTS enrollments;
+DROP TABLE IF EXISTS courses;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS roles;
+
 CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    role_name VARCHAR(100)
-);
+    role_name VARCHAR(100) NOT NULL UNIQUE
+) ENGINE=InnoDB;
 
 INSERT INTO roles (role_name) VALUES ('Student'), ('Instructor');
 
@@ -11,25 +16,25 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES roles (id)
-);
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+) ENGINE=InnoDB;
 
 CREATE TABLE courses (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    course_name VARCHAR(150)
-);
+    course_name VARCHAR(150) NOT NULL UNIQUE
+) ENGINE=InnoDB;
 
 INSERT INTO courses (course_name) VALUES ("Math"), ("Programming");
 
 CREATE TABLE enrollments (
-    id INT PRIMARY KEY AUTO_INCREMENT,   
-    user_id int,
-    grade int CHECK (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    grade INT CHECK (
         grade >= 0
         AND grade <= 100
     ),
-    course_id int UNIQUE,
-    FOREIGN KEY (course_id) REFERENCES courses (id),
-    FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
+    course_id INT,
+    UNIQUE (user_id, course_id),
+    FOREIGN KEY (course_id) REFERENCES courses (id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+) ENGINE=InnoDB;
