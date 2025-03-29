7767fdaca28c689548583163e47df6996af860c2
Pmacdon15 <pmacdonald15@gmail.com>
2025-03-29 15:00:28 -0600

    added login to decode auth_token cookie from client

--- v2/auth.php
+++ v2/auth.php
@@ -7,7 +7,13 @@ use Firebase\JWT\Key;
 include "config.php"; // $jwt_secret_key
 
 $headers = apache_request_headers();
-$jwt = isset($headers["Authorization"]) ? trim(str_replace("Bearer", "", $headers["Authorization"])) : "";
+$auth_cookie = isset($_COOKIE['auth_token']) ? $_COOKIE['auth_token'] : '';
+
+if ($auth_cookie) {
+    $jwt = $auth_cookie;
+} else {
+    $jwt = isset($headers['Authorization']) ? trim(str_replace("Bearer", "", $headers['Authorization'])) : '';
+}
 
 if (!$jwt) {
     die(json_encode(["error" => "No token provided"]));
